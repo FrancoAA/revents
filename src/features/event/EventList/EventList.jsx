8a781cf03b0339ab434e61c4a7840daf9f23045f
Franco <franco.alarcon@gmail.com>
2018-11-25 21:53:56 -0300

    adds mockup data

--- src/features/event/EventList/EventList.jsx
+++ src/features/event/EventList/EventList.jsx
@@ -4,12 +4,13 @@ import EventListItem from './EventListItem';
 
 class EventList extends Component {
   render() {
+    const { events } = this.props;
     return (
       <div>
         <h1>Event List</h1>
-        <EventListItem></EventListItem>
-        <EventListItem></EventListItem>
-        <EventListItem></EventListItem>
+        {
+          events.map(e => (<EventListItem key={e.id} event={e}></EventListItem>))
+        }
       </div>
     )
   }
