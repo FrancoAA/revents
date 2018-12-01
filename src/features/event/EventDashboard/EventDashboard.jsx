9577e605f1ccf02abd856990e899ad1900c5e2af
Franco <franco.alarcon@gmail.com>
2018-12-01 11:19:04 -0300

    converts event page to redux

--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -1,65 +1,28 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import { createEvent, updateEvent, deleteEvent } from '../eventActions';
+
 import { Grid, Button } from 'semantic-ui-react';
 import EventList from '../EventList/EventList';
 import EventForm from '../EventForm/EventForm';
 import cuid from 'cuid';
 
 
+const mapState = (state) => ({
+  events: state.events
+});
+
+const actions = {
+  createEvent,
+  updateEvent,
+  deleteEvent
+};
+
 class EventDashboard extends Component {
 
   state = {
     isOpen : false,
-    selectedEvent : null,
-    events : [
-      {
-        id: '1',
-        title: 'Trip to Tower of London',
-        date: '2018-03-27',
-        category: 'culture',
-        description:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.',
-        city: 'London, UK',
-        venue: "Tower of London, St Katharine's & Wapping, London",
-        hostedBy: 'Bob',
-        hostPhotoURL: 'https://randomuser.me/api/portraits/men/20.jpg',
-        attendees: [
-          {
-            id: 'a',
-            name: 'Bob',
-            photoURL: 'https://randomuser.me/api/portraits/men/20.jpg'
-          },
-          {
-            id: 'b',
-            name: 'Tom',
-            photoURL: 'https://randomuser.me/api/portraits/men/22.jpg'
-          }
-        ]
-      },
-      {
-        id: '2',
-        title: 'Trip to Punch and Judy Pub',
-        date: '2018-03-28',
-        category: 'drinks',
-        description:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.',
-        city: 'London, UK',
-        venue: 'Punch & Judy, Henrietta Street, London, UK',
-        hostedBy: 'Tom',
-        hostPhotoURL: 'https://randomuser.me/api/portraits/men/22.jpg',
-        attendees: [
-          {
-            id: 'b',
-            name: 'Tom',
-            photoURL: 'https://randomuser.me/api/portraits/men/22.jpg'
-          },
-          {
-            id: 'a',
-            name: 'Bob',
-            photoURL: 'https://randomuser.me/api/portraits/men/20.jpg'
-          }
-        ]
-      }
-    ]
+    selectedEvent : null
   }
 
   handleFormOpen = () => {
@@ -76,13 +39,8 @@ class EventDashboard extends Component {
   }
 
   handleUpdateEvent = (updatedEvent) => {
+    this.props.updateEvent(updatedEvent);
     this.setState({
-      events : this.state.events.map(event => {
-        if (event.id === updatedEvent.id) {
-          return Object.assign({}, updatedEvent);
-        }
-        return event;
-      }),
       isOpen : false,
       selectedEvent : null
     });
@@ -98,27 +56,24 @@ class EventDashboard extends Component {
   handleCreateEvent = (newEvent) => {
     newEvent.id = cuid();
     newEvent.hostPhotoURL = '/assets/user.png';
-    const updatedEvents = [...this.state.events, newEvent];
+    this.props.createEvent(newEvent);
     this.setState({
-      events : updatedEvents,
       isOpen : false
     });
   }
 
   handleDeleteEvent = (eventId) => () => {
-    const updatedEvents = this.state.events.filter(event => event.id !== eventId);
-    this.setState({
-      events : updatedEvents
-    });
+    this.props.deleteEvent(eventId);
   }
 
   render() {
     const { selectedEvent } = this.state;
+    const { events } = this.props;
 
     return (
       <Grid>
         <Grid.Column width={10}>
-          <EventList deleteEvent={this.handleDeleteEvent} events={this.state.events} onEventOpen={this.handleEditEvent}></EventList>
+          <EventList deleteEvent={this.handleDeleteEvent} events={events} onEventOpen={this.handleEditEvent}></EventList>
         </Grid.Column>
         <Grid.Column width={6}>
           <Button positive content="Create Event" onClick={this.handleFormOpen}></Button>
@@ -134,4 +89,4 @@ class EventDashboard extends Component {
   }
 }
 
-export default EventDashboard;
+export default connect(mapState, actions)(EventDashboard);
